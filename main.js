df2c763390cf2e6f289aaaa01ab74fa35893bdd6
mustot <90108839+mustot@users.noreply.github.com>
2021-11-11 19:44:33 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// Alert
+alert("Hello dear visitors.")
+
+// Prompt
+var visitorName = prompt("Please indicate your name.")
+
+// Log
+console.log(visitorName)
+
+// Confirm
+var iscoolwhitit = confirm("Do you agree to collect your data ?")
+
+// Console
+console.log(iscoolwhitit)
+
